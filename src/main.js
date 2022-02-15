19f6d69f0700086f3a6eed747cc74c10c2665df5
kaviarasanmani <64035875+kaviarasanmani@users.noreply.github.com>
2022-02-15 11:34:24 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 
-axios.defaults.baseURL = "http://127.0.0.1:8000"
+axios.defaults.baseURL = "https://erp-dashboard.herokuapp.com/"
 
 createApp(App).use(store).use(router, axios).mount("#app")
